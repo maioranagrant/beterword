28406a937cf0baadf014f3d3bef2ec6f8adfda62
maioranagrant <75107241+maioranagrant@users.noreply.github.com>
2022-04-18 21:12:35 -0400

    Add files via upload

---
+++ login.js
@@ -0,0 +1,17 @@
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
+loginErrorMsg.style.opacity = 0;
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+
+    if (username === "grant" && password === "1234") {
+        //alert("You have successfully logged in.");
+        window.location.href = "game.html";
+    } else {
+        loginErrorMsg.style.opacity = 1;
+    }
+});
---
+++ main.js
@@ -0,0 +1,116 @@
+
+if( document.readyState !== 'loading' ) {
+    console.log( 'document is already ready, just execute code here' );
+    initCode();
+} else {
+    document.addEventListener('DOMContentLoaded', function () {
+        console.log( 'document was not ready, place code here' );
+        initCode();
+    });
+}
+
+var audio;
+var lastAnswer;
+var tossupNum;
+var files;
+var answers;
+var startTime;
+var nowTime;
+
+
+function initCode()
+{
+
+    console.log("here11");
+    tossupNum = 0;
+    audio;
+    lastAnswer; 
+    files = new Array();
+    answers = new Array();
+
+    
+    
+    
+    answers[0] = 'American Gothic';
+    answers[1] = 'Tchaikovsky';
+    answers[2] = 'Oxidation';
+    answers[3] = 'Melville';
+    answers[4] = 'Bolivar';
+    
+    
+    
+    for(let i = 0; i < 5; i++)
+    {
+        files[i] = new Audio('Tossups\\' + (i+1).toString() + '.mp3');
+        
+        
+    }
+    document.getElementById("startButton").addEventListener("click",playTossup());
+    
+    document.getElementById("nextButton").style.visibility = "hidden";
+    
+    document.getElementById("enterAnswer").style.visibility = "hidden";
+    document.getElementById("correctDisplay").style.visibility = "hidden";
+    document.getElementById("celerityDisplay").style.visibility = "hidden";
+    document.getElementById("scoreDisplay").style.visibility = "hidden";
+    console.log("here11");
+}
+
+function playTossup()
+{
+    audio = files[tossupNum];
+    console.log('here');
+    audio.muted = false;
+    audio.play();
+    startTime = Date.now();
+
+}
+function buzz()
+{
+    nowTime = Date.now();
+    audio.pause();
+    
+    document.getElementById("enterAnswer").style.visibility = "visible";
+}
+function nextTossup()
+{
+    tossupNum++;
+    document.getElementById("enterAnswer").style.visibility = "hidden";
+    document.getElementById("correctDisplay").style.visibility = "hidden";
+    document.getElementById("celerityDisplay").style.visibility = "hidden";
+    document.getElementById("scoreDisplay").style.visibility = "hidden";
+}
+function evaluateAnswer()
+{
+    if (document.getElementById("answer").value == (answers[tossupNum]))
+    {
+        lastAnswer = true;
+        document.getElementById("correctDisplay").innerHTML = "Correct!";
+        document.getElementById("correctDisplay").style.visibility = "visible";
+        document.getElementById("celerityDisplay").style.visibility = "visible";
+        document.getElementById("scoreDisplay").style.visibility = "visible";
+
+        var deltaTime = nowTime - startTime;
+        var celer = 1.0 - ((deltaTime/1000.0) / audio.duration);
+        celer = celer.toFixed(3)
+        var score = Math.round(10 + (10 * celer));
+        if (celer < 0)
+        {
+            celer = 0;
+        }
+        document.getElementById("scoreDisplay").innerHTML = "Score: " + score + " points";
+        document.getElementById("celerityDisplay").innerHTML = "Celerity: " + celer;
+    }
+    else
+    {
+        lastAnswer = false;
+        document.getElementById("correctDisplay").innerHTML = "Incorrect!";
+        document.getElementById("correctDisplay").style.visibility = "visible";
+    }
+    document.getElementById("nextButton").style.visibility = "visible";
+
+
+    
+}
+
+
