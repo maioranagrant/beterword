184482b40900df16691db2ed7654a233071a3cbf
maioranagrant <75107241+maioranagrant@users.noreply.github.com>
2022-05-22 11:58:32 -0400

    Week 5 Launch

--- leaderboard.js
+++ leaderboard.js
@@ -7,7 +7,7 @@ if( document.readyState !== 'loading' ) {
     var w = localStorage.getItem("week");
         if (w === null)
         {
-            w = 4;
+            w = 5;
         }
         fillTable(w);
     
@@ -16,7 +16,7 @@ if( document.readyState !== 'loading' ) {
         var w = localStorage.getItem("week");
         if (w === null)
         {
-            w = 4;
+            w = 5;
         }
         fillTable(w);
     });
@@ -100,7 +100,7 @@ function launchComp()
 {
     user =  JSON.parse(localStorage.getItem("user"));
     var result = user["eml"];
-    firebase.database().ref('openedweek4').on('value', function(snap){
+    firebase.database().ref('openedweek5').on('value', function(snap){
         snap.forEach(function(childNodes){
             if (childNodes.val().name === "placeholder")
             {
